c13e1c79cd21543d42c73b27c869e57b8026d753
Amrit Kumar Chanchal <105360504+AmritKrChanchal@users.noreply.github.com>
2023-09-17 10:14:56 +0000

    data types

---
+++ .devcontainer/01_basics/dataTypes.js
@@ -0,0 +1,26 @@
+" use strict ";
+//  alert (3+3 )  we  are using node js Not Browzer So We cant use this 
+
+//console.log(3+3 );
+
+//console.log( "amrit" );
+
+
+let name = "amrit"
+let age = "18"
+
+
+
+//number
+//bolean
+//null
+//undefined
+//symbol  unique features 
+//
+
+//console.log(typeof age);
+
+
+let score =33
+
+console.log (typeof score)
---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId =144556
+let accountEmail = "amrit@gmail.com"
+let accountState 
+
+//accountId = 455 Not Allowed in clading we cant change
+console.log(accountId);
+
+
+accountEmail ="amrit@google.com"
+
+console.table([accountEmail , accountId , accountState ])
